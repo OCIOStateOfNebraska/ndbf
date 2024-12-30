02dde0cd2150628698abbda388fa70030e8915a1
NetKuta <matt.kuta@nebraska.gov>
2024-12-30 08:07:36 -0600

    renamed to link brick

--- blocks/link-bricks/link-bricks.js
+++ blocks/link-bricks/link-bricks.js
@@ -3,18 +3,38 @@
  */
 
 export default function decorate(block) {
+    const columnRows = 0;
     [...block.children].forEach((row) => {
         // decorate link brick item label
         const linkbrick = document.createElement('div');
         linkbrick.className = 'linkbrick-item';
+       
+        const brickDesc = brickDesc.firstElementChild ? brickDesc.firstElementChild.innerText : '';
+        if (brickDesc.toLowerCase().includes('small')) {
+    
+        }
+
         const linkChild = row.lastElementChild;
-        
         const label = linkChild.firstElementChild ? linkChild.firstElementChild.innerText : '';
         const linkHref = linkChild.firstElementChild && linkChild.firstElementChild.lastElementChild ? linkChild.firstElementChild.lastElementChild.href : '#';
+
         const link = document.createElement('a');
         link.href = linkHref;
         const linkDiv = document.createElement('div');
-        linkDiv.className = 'linkbrick-item-red';
+
+        if(brickDesc.toLowerCase().includes('green')) {
+            linkDiv.className = 'linkbrick-green';
+        }
+        else if(brickDesc.toLowerCase().includes('red')) {
+            linkDiv.className = 'linkbrick-red';
+        }
+        else if(brickDesc.toLowerCase().includes('gray')) {
+            linkDiv.className = 'linkbrick-gray';
+        }
+        else {
+            linkDiv.className = 'linkbrick-default';
+        }
+
         const linkSpan = document.createElement('span');
         linkSpan.textContent = label;
         linkDiv.append(linkSpan);
