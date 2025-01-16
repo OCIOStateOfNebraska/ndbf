86176422b60b5dcc9b9853bf239bce275a8a86a6
NetKuta <matt.kuta@nebraska.gov>
2025-01-16 15:14:21 -0600

    updated some blocks

--- blocks/link-bricks/link-bricks.js
+++ blocks/link-bricks/link-bricks.js
@@ -5,34 +5,38 @@
 export default function decorate(block) {
     let columnRows = 0;
     let column = document.createElement('div');
+    column.className = 'col-6';
     const mainDiv = document.createElement('div');
+    mainDiv.className='row';
 
     [...block.children].forEach((row) => {
         // decorate link brick item label
         let colAdd = 0;
 
         const brickDesc = row.firstElementChild ? row.firstElementChild.innerText : '';
-
+        
+        const linkbrick = document.createElement('div');
+        
         if (brickDesc.toLowerCase().includes('small')) {
             colAdd = 1;
+            linkbrick.className = 'linkbrick-item small';
         }
         else if (brickDesc.toLowerCase().includes('large')) {
             colAdd = 2;
+            linkbrick.className = 'linkbrick-item large';
         }
 
         if (columnRows + colAdd > 2)
         {
             column = document.createElement('div');
+            column.className = 'col-6';
             columnRows = 0;
         }
         else
         {
             columnRows += colAdd;
         }
-
-        const linkbrick = document.createElement('div');
-        linkbrick.className = 'linkbrick-item';
-
+ 
         const linkChild = row.lastElementChild;
         const label = linkChild.firstElementChild ? linkChild.firstElementChild.innerText : '';
         const linkHref = linkChild.firstElementChild && linkChild.firstElementChild.lastElementChild ? linkChild.firstElementChild.lastElementChild.href : '#';
@@ -55,6 +59,7 @@ export default function decorate(block) {
         }
 
         const linkSpan = document.createElement('span');
+        linkSpan.className="h2";
         linkSpan.textContent = label;
         linkDiv.append(linkSpan);
         link.append(linkDiv);
