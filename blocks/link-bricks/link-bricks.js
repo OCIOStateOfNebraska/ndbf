15510944593bf3b0ff7aac5993510fa2cc73ca84
NetKuta <matt.kuta@nebraska.gov>
2025-01-17 11:25:02 -0600

    more lint fixes

--- blocks/link-bricks/link-bricks.js
+++ blocks/link-bricks/link-bricks.js
@@ -15,53 +15,52 @@ export default function decorate(block) {
     const brickDesc = row.firstElementChild ? row.firstElementChild.innerText : '';
     const linkbrick = document.createElement('div');
     if (brickDesc.toLowerCase().includes('small')) {
-        colAdd = 1;
-        linkbrick.className = 'linkbrick-item small';
+      colAdd = 1;
+      linkbrick.className = 'linkbrick-item small';
     }
     else if (brickDesc.toLowerCase().includes('large')) {
-        colAdd = 2;
-        linkbrick.className = 'linkbrick-item large';
+      colAdd = 2;
+      linkbrick.className = 'linkbrick-item large';
     }
 
     if (columnRows + colAdd > 2) {
-        column = document.createElement('div');
-        column.className = 'col-6';
-        columnRows = 0;
+      column = document.createElement('div');
+      column.className = 'col-6';
+      columnRows = 0;
     }
-    else
-    {
-        columnRows += colAdd;
+    else{
+      columnRows += colAdd;
     }
  
-        const linkChild = row.lastElementChild;
-        const label = linkChild.firstElementChild ? linkChild.firstElementChild.innerText : '';
-        const linkHref = linkChild.firstElementChild && linkChild.firstElementChild.lastElementChild ? linkChild.firstElementChild.lastElementChild.href : '#';
+    const linkChild = row.lastElementChild;
+    const label = linkChild.firstElementChild ? linkChild.firstElementChild.innerText : '';
+    const linkHref = linkChild.firstElementChild && linkChild.firstElementChild.lastElementChild ? linkChild.firstElementChild.lastElementChild.href : '#';
 
-        const link = document.createElement('a');
-        link.href = linkHref;
-        const linkDiv = document.createElement('div');
+    const link = document.createElement('a');
+    link.href = linkHref;
+    const linkDiv = document.createElement('div');
 
-        if(brickDesc.toLowerCase().includes('green')) {
-            linkDiv.className = 'linkbrick-green';
-        }
-        else if(brickDesc.toLowerCase().includes('red')) {
-            linkDiv.className = 'linkbrick-red';
-        }
-        else if(brickDesc.toLowerCase().includes('gray')) {
-            linkDiv.className = 'linkbrick-gray';
-        }
-        else {
-            linkDiv.className = 'linkbrick-default';
-        }
+    if(brickDesc.toLowerCase().includes('green')) {
+      linkDiv.className = 'linkbrick-green';
+    }
+    else if(brickDesc.toLowerCase().includes('red')) {
+      linkDiv.className = 'linkbrick-red';
+    }
+    else if(brickDesc.toLowerCase().includes('gray')) {
+      linkDiv.className = 'linkbrick-gray';
+    }
+    else {
+      linkDiv.className = 'linkbrick-default';
+    }
 
-        const linkSpan = document.createElement('span');
-        linkSpan.className="h2";
-        linkSpan.textContent = label;
-        linkDiv.append(linkSpan);
-        link.append(linkDiv);
-        linkbrick.append(link);
-        column.append(linkbrick);
-        mainDiv.append(column);
-        row.replaceWith(mainDiv);
-    });
+    const linkSpan = document.createElement('span');
+    linkSpan.className="h2";
+    linkSpan.textContent = label;
+    linkDiv.append(linkSpan);
+    link.append(linkDiv);
+    linkbrick.append(link);
+    column.append(linkbrick);
+    mainDiv.append(column);
+    row.replaceWith(mainDiv);
+  });
 }
