98e31cb99b2b5a76f8a2c971d9a06d744b762f2f
Jesstradiol <jessica.elsener@Nebraska.gov>
2025-03-07 11:37:40 -0600

    css to colums for icons

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -16,23 +16,4 @@ export default function decorate(block) {
       }
     });
   });
-  const aTags = document.querySelectorAll('a');
-  const fragment = document.createDocumentFragment();
-  aTags.forEach((aTag) => {
-    const href = aTag.getAttribute('href');
-    if (href.endsWith('.pdf') || href.endsWith('.jpg') || href.endsWith('.svg')) {
-      const spanElement = document.createElement('span');
-      spanElement.classList = 'icon icon-file-image';
-      spanElement.style = 'mask-image: url("/icons/file-image.svg")';
-      fragment.appendChild(spanElement);
-      aTag.parentNode.insertBefore(spanElement, aTag);
-    } else {
-      const spanElement = document.createElement('span');
-      spanElement.classList = 'icon icon-file-image';
-      spanElement.style = 'mask-image: url("/icons/file.svg")';
-      fragment.appendChild(spanElement);
-      aTag.parentNode.insertBefore(spanElement, aTag);
-    }
-  });
-  document.body.appendChild(fragment);
 }
