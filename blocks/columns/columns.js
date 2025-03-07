4c9413538efd551651112b0bc87ad56112bc977b
Jesstradiol <jessica.elsener@Nebraska.gov>
2025-03-07 08:49:33 -0600

    added pdf js to Columns js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,5 +1,6 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
+
   block.classList.add(`columns-${cols.length}-cols`);
 
   // setup image columns
@@ -15,4 +16,23 @@ export default function decorate(block) {
       }
     });
   });
+  const aTags = document.querySelectorAll('a');
+  const fragment = document.createDocumentFragment();
+  aTags.forEach((aTag) => {
+    const href = aTag.getAttribute('href');
+    if (href.endsWith('.pdf') || href.endsWith('.jpg') || href.endsWith('.svg')) {
+      const spanElement = document.createElement('span');
+      spanElement.classList = 'icon icon-file-image';
+      spanElement.style = 'mask-image: url("/icons/file-image.svg")';
+      fragment.appendChild(spanElement);
+      aTag.parentNode.insertBefore(spanElement, aTag);
+    } else {
+      const spanElement = document.createElement('span');
+      spanElement.classList = 'icon icon-file-image';
+      spanElement.style = 'mask-image: url("/icons/file.svg")';
+      fragment.appendChild(spanElement);
+      aTag.parentNode.insertBefore(spanElement, aTag);
+    }
+  });
+  document.body.appendChild(fragment);
 }
